51b57b83958a4879e2634f63044be5d14ac6c746
Robert Guan <guan.robert@utexas.edu>
2021-01-30 23:03:23 -0600

    added tree script on event system in main scene

---
+++ Assets/Scripts/TreeRandomizer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeRandomizer : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> treesList;
+    [SerializeField] private float treeSideTiltLimit;
+    [SerializeField] private float treeHeight;
+    void Start()
+    {
+        foreach(GameObject tree in treesList)
+        {
+            
+        }
+    }
+    private void MakeTreeRandom()
+    {
+
+    }
+
+
+}
