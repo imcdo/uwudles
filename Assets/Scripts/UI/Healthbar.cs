b3f229fb42bbecbe7b071dc901dc32cf90b6383e
Mindy Chi <mindychi@gmail.com>
2021-01-31 13:02:34 -0600

    Health number

--- Assets/Scripts/UI/Healthbar.cs
+++ Assets/Scripts/UI/Healthbar.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace uwudles.UI
 {
+
     [RequireComponent(typeof(RectTransform))]
     public class Healthbar : MonoBehaviour
     {
+        public TextMeshProUGUI healthNum;
         private RectTransform _rt;
         private float _fullX;
         [SerializeField] private Damagable _damagable;
@@ -18,10 +21,12 @@ namespace uwudles.UI
             _rt = GetComponent<RectTransform>();
             _fullX = _rt.rect.width * _rt.localScale.x;
             _damagable.HpListener.AddListener(UpdateBar);
+            healthNum.text = ((int) _damagable.Hp).ToString();
         }
 
         private void UpdateBar()
         {
+            healthNum.text = ((int) _damagable.Hp).ToString();
             float fullness = 1.0f - (float)_damagable.Hp / _damagable.MaxHp;
             _rt.sizeDelta = new Vector2(-fullness * _fullX, _rt.sizeDelta.y);
             
