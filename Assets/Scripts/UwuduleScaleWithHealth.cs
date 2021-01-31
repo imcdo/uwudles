cfeba82a78b3939c11020bef3c4f44798177c8ae
digocorbellini <digocorbellini@gmail.com>
2021-01-31 03:19:04 -0600

    Scale with current health

--- Assets/Scripts/UwuduleScaleWithHealth.cs
+++ Assets/Scripts/UwuduleScaleWithHealth.cs
@@ -10,8 +10,14 @@ namespace uwudles
         [SerializeField] private float scaleForSize;
         private Damagable _health;
         public Damagable Health => _health ? _health : _health = GetComponent<Damagable>();
+        
 
         private void Start() {
+            updateScale();
+            Health.HpListener.AddListener(updateScale);
+        }
+
+        private void updateScale(){
             float scale = Health.Hp/scaleForSize;
             transform.localScale *= scale;
         }
